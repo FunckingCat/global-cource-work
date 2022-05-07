faa047400fc9e1a81800aa83aa09143551d14460
David <david99111@mail.ru>
2022-05-07 12:24:35 +0300

    base_template_works

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('coupons/', include('main.urls')),
     path('auth/', include('regauth.urls'))
 ]
